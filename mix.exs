a9fc5a27b99fd15acbf434df3fdc48b8849e3478
= <=>
2016-05-27 16:05:04 -0400

    version update

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Chatrooms.Mixfile do
 
   def project do
     [app: :chatrooms,
-     version: "0.0.3",
+     version: "0.0.4",
      elixir: "~> 1.0",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
